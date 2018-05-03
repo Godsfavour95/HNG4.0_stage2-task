2164eab4066aef3ffc0a4fe9197c45794f66a5a8
Godsfavour95 <odomgodsfavour@gmail.com>
2018-05-03 12:25:59 +0100

    reinitialized files

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>FAMILY FUN </title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		
+		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+	
+	
+	
+	</head>
+	<body>
+		
+			<header>
+				<div class="sitename">
+					<h1 class="red">Family<span> Fun</span></h1> 
+					<nav>
+					<ul class="navigation">
+						  <li><a href="#">Home</a></li>
+						  <li><a href="#">About</a></li>
+						  <li><a href="#">Contact</a></li>
+					</ul>
+				   </nav>
+				</div>
+			
+			
+			</header>
+			
+			<section class="background">
+				
+			<img class="pics" src="images/Black-Family.jpg">
+			
+			</section>
+			
+			<section class="redbackground">
+				
+			<p class="talk">We make your family comfortable</p>
+			
+			</section>
+			
+			
+			
+			<section class="write-up">
+				<div class="left-writeup">
+					<p> Ever thought of how to give your family the best?</p>
+				</div>
+				
+				<div class="right-writeup">
+					<p class="red-date"><?php date_default_timezone_set("Africa/Lagos"); echo date(" g:i a"); ?> </p> 
+					<h3> Keep a date with us...</h3>
+				</div>
+				
+				
+			</section>
+			
+			<footer class="footer">
+				
+				<h1 class="footer-textcolor">Stable</h1>
+				<p class="footer-textcolor">Website designed by Template Monster.com</p>
+			
+			</footer>
+		
+	
+	</body>
+
+
+
+</html>
